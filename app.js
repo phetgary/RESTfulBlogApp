6ca14be12e2265c8198c516354a2f80ae455b6f7
phetgary <56030176+phetgary@users.noreply.github.com>
2020-06-04 16:37:18 +0800

    add edit/update route

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    express     = require("express"),
-    app         = express();
+var bodyParser      = require("body-parser"),
+    methodOverride  = require("method-override"),
+    mongoose        = require("mongoose"),
+    express         = require("express"),
+    app             = express();
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 var blogSchmea = new mongoose.Schema({
     title: String,
@@ -61,9 +63,29 @@ app.get("/blogs/:id", function(req, res){
     });
 });
 
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
 
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
 
-
+app.delete("/blogs/:id", function(req, res){
+    res.send("You have reached the delete page");
+});
 
 app.listen(3000, function(){
     console.log("SERVER IS RUNNING!");
